c9191a561dea800d634e0db433e07f96dba85b82
Administrator <Administrator@www.zh.cao.com>
2015-11-27 16:37:33 +0800

    app 主题  名字 配置

--- qbcf/src/com/ujuit/qbcf/activity/MainActivity.java
+++ qbcf/src/com/ujuit/qbcf/activity/MainActivity.java
@@ -1,11 +1,13 @@
 package com.ujuit.qbcf.activity;
 
+import com.anthole.quickdev.invoke.SystemBarTintInvoke;
 import com.ujuit.qbcf.R;
 import com.ujuit.qbcf.activity.base.BaseActivity;
+import com.ujuit.qbcf.activity.base.BaseSwipeActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseSwipeActivity {
 
 	@Override
 	protected int getLayoutId() {
@@ -15,5 +17,10 @@ public class MainActivity extends BaseActivity {
 	@Override
 	protected void initData(Bundle savedInstanceState) {
 		
+	}
+
+	@Override
+	public void afterInitSwipe() {
+		SystemBarTintInvoke.apply(this, R.color.red);
 	}
 }
